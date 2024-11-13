e8ae8a3b0fde57678bde30f1cd3ecb29281b2cee
SofieJegind <soch22ad@student.cbs.dk>
2024-11-13 14:03:11 +0100

    Update shoppinglist

--- screens/StackScreens/ShoppingList.js
+++ screens/StackScreens/ShoppingList.js
@@ -1,6 +1,6 @@
 // ShoppingListScreen.js
 import React from 'react';
-import { View, Text, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, SafeAreaView, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import GGlogoComponent from '../../components/GGlogo';
 import GlobalStyles from '../../styles/GlobalStyles';
 import { useState, useEffect } from 'react';
@@ -48,9 +48,24 @@ export default function ShoppingList({route}) {
     if (!item.Produkt|| !item.Produkt) return null;
 
     return (
-    <View style={[GlobalStyles.productItem, { flexDirection: "row", justifyContent: "space-between" }]}>
-      <Text style={GlobalStyles.text}>{item.Produkt}</Text>
-      <RemoveButtonComponent productName={item.Produkt} setShoppingList={setShoppingList} />
+    <View style={styles.productCard}>
+    <View style={[GlobalStyles.productItem, { flexDirection: "row", justifyContent: "center" }]}>
+      
+      <View style={{flexDirection: "column"}}>
+      <Text style={[GlobalStyles.text, {right: 90}]}>{item.Produkt}</Text>
+      <Text style={[GlobalStyles.smallText, {right:90}]}>CO2 aftryk:</Text>
+      </View>
+
+      
+      <View style={styles.checkboxStyle}>
+      <View style={styles.checkbox}>
+          <Text style={styles.checkboxIcon}>✓</Text> 
+      </View>
+      <RemoveButtonComponent productName={item.Produkt} setShoppingList={setShoppingList} style={styles.deletebtn}/>
+      </View>
+
+
+      </View>
     </View>
   );
 }
@@ -74,3 +89,55 @@ export default function ShoppingList({route}) {
   );
 }
 
+const styles = StyleSheet.create({
+  productCard: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 16,
+    marginVertical: 8,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 2,
+    width: 350,
+  },
+  productTextContainer: {
+    flexDirection: 'column',
+  },
+  productName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  co2Text: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 4,
+    right: 90, 
+  },
+  checkbox: {
+    width: 40,
+    height: 40,
+    borderWidth: 2,
+    borderColor: '#3c763d',
+    borderRadius: 12,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  checkboxStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    left: 100,
+  },
+  checkboxIcon: {
+    fontSize: 16,
+    color: '#3c763d',
+  },
+  deletebtn: {
+    top: 50,
+  },
+
+})
